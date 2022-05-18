282f91179b907c8b3eb09723d340516e5ecb01ce
awkto <awkto@outlook.com>
2022-05-18 14:15:39 -0400

    Modified create script to save json as a local dotfile

--- create-kube-cluster.sh
+++ create-kube-cluster.sh
@@ -9,6 +9,8 @@ curl -s -X POST "https://api.digitalocean.com/v2/kubernetes/clusters" \
 	-H "Authorization: Bearer $DTOKEN" \
 	-H "Content-Type: application/json" \
 	-d @$PAYLOAD \
-	| jq .
+	| jq . | tee .kube-cluster-json | jq .
+
+echo "Cluster ID is : "$(cat .kube-cluster-json | jq .kubernetes_cluster.id | tee .kube-cluster-id)
 
 
